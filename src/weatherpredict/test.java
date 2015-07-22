df04d9799c8e975aefd7191ee201855e2157f302
vietlink <vlink91@gmail.com>
2015-07-23 00:33:25 +0700

    hoàn thiện thuật toán

---
+++ src/weatherpredict/test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weatherpredict;
+
+/**
+ *
+ * @author ngo
+ */
+public class test {
+    public static void main(String[] args) {
+        float a= (float) 4.5;
+        double b= 2.2;
+        System.out.println(Math.pow(a, b));
+    }
+}
